a95550b6777e6d356e6740fb8218745b93ba07ee
Anna Hernandez <annahernandezduran@gmail.com>
2023-02-19 11:23:27 +0000

    fixed typo in noxfile

--- noxfile.py
+++ noxfile.py
@@ -5,20 +5,20 @@ locations = ["src"]
 
 @nox.session(python=["3.8"])
 def black(session):
-    args = sessions.posargs or locations
+    args = session.posargs or locations
     session.install("black")
     session.run("black", "--line-length", "79", *args)
 
 
 @nox.session(python=["3.8"])
 def flake8(session):
-    args = sessions.posargs or locations
+    args = session.posargs or locations
     session.install("flake8")
     session.run("flake8", *args)
 
 
 @nox.session(python=["3.8"])
 def isort(session):
-    args = sessions.posargs or locations
+    args = session.posargs or locations
     session.install("isort")
     session.run("isort", *args)
