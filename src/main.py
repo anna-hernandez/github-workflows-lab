7fea3d80b2e958917dc79749a129ac5c82283ea9
Anna Hernandez <annahernandezduran@gmail.com>
2023-02-19 17:49:47 +0000

    changed message to test Actions triggers for unit and integration tests

--- src/main.py
+++ src/main.py
@@ -6,4 +6,9 @@ def print_greeting(message):
 
 if __name__ == "__main__":
     message = "Hi there! this repo is set up to test GitHub Actions workflows"
+    message = (
+        "Second message to test that only unit and integration tests",
+        " are run through Actions when it's a pull request to develop",
+        " or main branches",
+    )
     print_greeting(message)
