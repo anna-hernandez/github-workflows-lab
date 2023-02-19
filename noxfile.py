a2134478bac534a69c54370769408206740213fd
Anna Hernandez <annahernandezduran@gmail.com>
2023-02-19 11:42:53 +0000

    added noxfile and requirements

---
+++ noxfile.py
@@ -0,0 +1,24 @@
+import nox
+
+locations = ["src"]
+
+
+@nox.session(python=["3.8"])
+def black(session):
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", "--line-length", "79", *args)
+
+
+@nox.session(python=["3.8"])
+def flake8(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
+
+
+@nox.session(python=["3.8"])
+def isort(session):
+    args = session.posargs or locations
+    session.install("isort")
+    session.run("isort", *args)
