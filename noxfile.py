9b54d5303172d2ec038cd8dc8ab542118b536b18
Anna Hernandez <annahernandezduran@gmail.com>
2023-02-19 11:22:08 +0000

    added flake8, isort to noxfile

--- noxfile.py
+++ noxfile.py
@@ -3,8 +3,22 @@ import nox
 locations = ["src"]
 
 
-@nox.session(python=["3.9"])
+@nox.session(python=["3.8"])
 def black(session):
-    args = locations
+    args = sessions.posargs or locations
     session.install("black")
     session.run("black", "--line-length", "79", *args)
+
+
+@nox.session(python=["3.8"])
+def flake8(session):
+    args = sessions.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
+
+
+@nox.session(python=["3.8"])
+def isort(session):
+    args = sessions.posargs or locations
+    session.install("isort")
+    session.run("isort", *args)
